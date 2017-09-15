2f9f29bac63f4cb4d380b514d50b468f910a1431
Dmitriy Kiyaev <dima.kiyaev@gmail.com>
2017-09-15 12:40:49 +0300

    fin changes v01

--- install.sh
+++ install.sh
@@ -40,7 +40,35 @@ ln -sf "$(pwd)/tmux/tmux.conf" ~/.tmux.conf
 
 #ZSH init
 
+zsh_ins=0
+grep -q "/bin/zsh" /etc/shells || zsh_ins=1
 
+if [ $zsh_ins = 0 ]; then
+    usr_zsh_ins=0
+    grep -q "/usr/bin/zsh" /etc/shells || usr_zsh_ins=1
+    if [ $usr_zsh_ins = 1 ]; then
+        echo "zsh already installed,if need add /etc/shell val= /usr/bin/zsh; "echo "/bin/zsh >> /etc/shells"""
+    else
+        echo "zsh already install for user"
+    fi
+else
+    echo "Need zsh install"
+fi
+
+#check user def shell and install oh-my-zsh
+
+if echo $SHELL | grep -q zsh ; then
+    if [ ! -d $HOME/.oh-my-zsh ]; then
+        echo oh-my-zsh not  installing...
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+        ln -sf "$(pwd)/zsh/zshrc" ~/.zshrc
+        echo Done!!
+    else
+        echo oh-my-zsh already install
+    fi
+else
+    chsh -s /bin/zsh
+fi
 
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 #git pull
