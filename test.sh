3574f658ca9398f6e38882650dfc9832c90d5b8d
Dmitriy Kiyaev <dima.kiyaev@gmail.com>
2017-09-15 12:35:12 +0300

    add zsh and oh-my-zsh check and inst

--- test.sh
+++ test.sh
@@ -11,14 +11,28 @@ if [ $zsh_ins = 0 ]; then
     usr_zsh_ins=0
     grep -q "/usr/bin/zsh" /etc/shells || usr_zsh_ins=1
     if [ $usr_zsh_ins = 1 ]; then
-        echo "zsh already installed, need add /etc/shell val= /usr/bin/zsh"
+        echo "zsh already installed,if need add /etc/shell val= /usr/bin/zsh; "echo "/bin/zsh >> /etc/shells"""
     else
-        echo "zsh install for user"
+        echo "zsh already install for user"
     fi
 else
     echo "Need zsh install"
 fi
 
+#check user def shell and install oh-my-zsh
+
+if echo $SHELL | grep -q zsh ; then
+    if [ ! -d $HOME/.oh-my-zsh ]; then
+        echo oh-my-zsh not  installing...
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+        ln -sf "$(pwd)/zsh/zshrc" ~/.zshrc
+        echo Done!!
+    else
+        echo oh-my-zsh already install
+    fi
+else
+    chsh -s /bin/zsh
+fi
 
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 #git pull
